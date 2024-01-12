a82e05b4225db3dd4c76b28772d3fefbdc4734da
Innocent Ingabire <inno100os@gmail.com>
2024-01-12 15:56:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+
+function App() {
+  return (
+    <>
+    <GlobalStyles/>
+    <div className="flex items-center justify-center">
+    <h1 className='text-colorBrand500 text-center'>Remotide</h1>
+      <Button>Remotide</Button>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        colorBrand500: "#6366f1",
+        colorBrand600: " #4f46e5",
+        colorBrand700: " #4338ca",
+        colorBrand50: "#eef2ff",
+        greyLogo: "#AFABAA",
+        defaultFontColor: "#374151",
+        greyBackground: "#F9FAFB",
+      },
+      textColor: {
+        default: "#374151",
+      },
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
